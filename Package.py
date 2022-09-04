d26f5470ff831619d15637b22ef0c6bd9539936f
AbisolaOseni <abisola.oseni@gmail.com>
2022-09-03 17:42:42 -0700

    Initial commit

---
+++ Package.py
@@ -0,0 +1,9 @@
+class Package:
+    def __init__(self, package_id, address, city, zipcode, deadline, package_weight, delivery_status):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.zipcode = zipcode
+        self.deadline = deadline
+        self.package_weight = package_weight
+        self.delivery_status = delivery_status
